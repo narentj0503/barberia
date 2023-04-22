0603059b5119c1a523993c59636fceb50652e1d3
Narent Gutierrez <gnarent@gmail.com>
2023-04-22 10:46:07 -0500

    proyecto barberia

---
+++ the barbershop/src/Home/Home.jsx
@@ -0,0 +1,16 @@
+import { Menu } from "../Menu/Menu"
+import "./Home.css"
+
+export function Home(){
+    return(
+        <>
+        <Menu>
+
+        </Menu>
+
+        <section className="banner text-white fw-bold">
+            <h2>THE BARBER SHOP</h2>
+        </section>
+        </>
+    )
+}
